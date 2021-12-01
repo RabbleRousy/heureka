4612968945ebd76c7bff326bb018d95ed77773e6
Simon Hetzer (desktop) <simon.hetzer@icloud.com>
2021-12-01 15:08:15 +0100

    Network matrices on heap instead of stack

--- NNUE.h
+++ NNUE.h
@@ -12,29 +12,56 @@ private:
 	static const int M = 256;
 	static const int K = 32;
 
-
 	// Linear network layers
 	template <int inputSize, int outputSize>
 	struct Linear {
-		float weights[inputSize][outputSize];
-		float biases[outputSize];
-	};
+		float** weights;
+		float* biases;
 
-	Linear<N, M> L0;
-	Linear<M*2, K> L1;
-	Linear<K, K> L2;
-	Linear<K, 1> L3;
+		Linear() {
+			// Construct the input x output Weights matrix
+			weights = new float* [inputSize];
+			for (int i = 0; i < inputSize; i++) {
+				weights[i] = new float[outputSize];
+			}
+
+			biases = new float[outputSize];
+		}
+
+		~Linear() {
+			for (int i = 0; i < inputSize; i++) {
+				delete[] weights[i];
+			}
+			delete[] weights;
+			delete[] biases;
+		}
+	};
 
 	struct Accumulator {
 		// These will be the input vector of L1
-		float v[2][M];
+		float* v[2];
 
 		float* operator[](bool white) {
 			return v[white];
 		}
+
+		Accumulator() {
+			v[0] = new float[M];
+			v[1] = new float[M];
+		}
+
+		~Accumulator() {
+			delete[] v[0];
+			delete[] v[1];
+		}
 	};
 
 	Accumulator accumulator;
+
+	Linear<N, M> L0;
+	Linear<M * 2, K> L1;
+	Linear<K, K> L2;
+	Linear<K, 1> L3;
 
 	void recalculateAccumulator(const std::vector<int> &activeFeatures, bool white);
 	void updateAccumulator(const std::vector<int>& removedFeatures, const std::vector<int>& addedFeatures, bool white);
