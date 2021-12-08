eb1806927ca55a0aea5973292cc16ee22c825ea5
Simon Hetzer (desktop) <simon.hetzer@icloud.com>
2021-12-08 18:01:02 +0100

    Experiments with mlpack layers

--- NNUE.cpp
+++ NNUE.cpp
@@ -70,7 +70,33 @@ void NNUE::train() {
 	// Cut the labels from the last row of the trainingData
 	arma::mat labels = (arma::mat)sparseMatrix.submat(sparseMatrix.n_rows - 1, 0, sparseMatrix.n_rows - 1, sparseMatrix.n_cols - 1);
 
-	mlpack::ann::NNUEnet network;
+	mlpack::ann::FFN<mlpack::ann::MeanSquaredError<>> network;
+
+	mlpack::ann::Sequential<> firstHalf(2 * N, M);
+	//firstHalf.Add<mlpack::ann::Subview<>>(1, 0, N - 1, 0, 0);
+	firstHalf.Add<mlpack::ann::Linear<>>(2 * N, M);
+	firstHalf.Add<mlpack::ann::ReLULayer<>>();
+
+	mlpack::ann::Sequential<> secondHalf(2 * N, M);
+	//secondHalf.Add<mlpack::ann::Subview<>>(1, N, 2 * N - 1, 0, 0);
+	secondHalf.Add<mlpack::ann::Linear<>>(2 * N, M);
+	secondHalf.Add<mlpack::ann::ReLULayer<>>();
+
+	mlpack::ann::Concat<> concat(2 * N, 2 * M);
+	concat.Add<mlpack::ann::Sequential<>>(firstHalf);
+	concat.Add<mlpack::ann::Sequential<>>(secondHalf);
+
+	// Add the concatenation to the network
+	network.Add<mlpack::ann::Concat<>>(concat);
+	// L1
+	network.Add<mlpack::ann::Linear<> >(2 * M, K);
+	network.Add<mlpack::ann::ReLULayer<> >();
+	// L2
+	network.Add<mlpack::ann::Linear<> >(K, K);
+	network.Add<mlpack::ann::ReLULayer<> >();
+	// L3
+	network.Add<mlpack::ann::Linear<> >(K, 1);
+	network.Add<mlpack::ann::ReLULayer<> >();
 
 	network.Train(data, labels);
 }
